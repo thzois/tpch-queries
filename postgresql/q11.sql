99e2cd6d2e506682f8171ad1b0eafc5c0bd97dc7
Thodoris Zois <thzois.csd@gmail.com>
2021-05-19 10:46:50 +0200

    Update q11.sql

--- postgresql/q11.sql
+++ postgresql/q11.sql
@@ -9,9 +9,7 @@ where ps_suppkey = s_suppkey
 	and n_name = 'GERMANY'
 group by ps_partkey 
 having sum(ps_supplycost * ps_availqty) >
-	( select sum(ps_supplycost * ps_availqty) * 0.0001000000
-			-- The above constant needs to be adjusted according
-			-- to the scale factor (SF): constant = 0.0001 / SF.
+	( select sum(ps_supplycost * ps_availqty) * FRAQ
 		from partsupp,
 			supplier,
 			nation
